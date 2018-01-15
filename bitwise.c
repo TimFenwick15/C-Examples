6a66f39080c65eae9935c97e995d04794c1a417a
TimFenwick15 <tjf20@hotmail.co.uk>
2018-01-15 17:29:10 +0000

    added a link for more information

--- bitwise.c
+++ bitwise.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// A good resource can be found at : http://www.how2lab.com/programming/c/low-level-programming.php
+
 int main (void) {
   typedef unsigned int uint;
   uint a = 0;
@@ -20,7 +22,12 @@ int main (void) {
   printf("1 << 2 is %d\n", 1 << 2);
   printf("0x1 << 2 is %d\n", 0x1 << 2);
 
+  printf("Sizeof something = %d\n", 8 * (int) sizeof(0x0CF00401));
+  printf("Sizeof some binary = %d\n", 8 * (int) sizeof(0b000000001111000000000100));
 
+  // How to apply a mask
+  // This didn't give the value I was expecting, I think it has something to do with endianness
+  printf("Masked value: %#010x\n", 0x0CF00401 & 0x03FFFF00);
   return 0;
 }
 
